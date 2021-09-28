e9a4ee47a18af3c98e82c3fc607e7f3674802c38
rinku <bangalerinku@gmail.com>
2021-09-28 21:38:18 +0530

    New Component to add

--- Front-end/redbus/src/App.js
+++ Front-end/redbus/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { Navbar } from "./Components/Navbar/Navbar";
 import Footer from "./Components/Footer/footer";
 import Number from "./Components/numberPage/number";
+import Travel from "./Components/Travels/Travels";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Number />
+      <Travel />
       <Footer />
     </div>
   );
---
+++ Front-end/redbus/src/Components/Travels/Travels.jsx
@@ -0,0 +1,139 @@
+import styles from './Travels.css'
+
+export default function Travel() {
+    
+    return (<>
+        <div className="seo-section">
+            <div className='main-body'>
+                <div className='seo-wrapper rest1 animate' style={{'display': 'flex'}}>
+                    <div className='tac'>
+                        <div className='seo-header'>Top Bus Routes</div>
+                        <a>Hyderabad to Bangalore Bus</a>
+                        <a>Bangalore to Chennai Bus</a>
+                        <a>Pune to Bangalore Bus</a>
+                        <a>Mumbai to Bangalore Bus</a>
+                        <a>{'More >'}</a>
+                    </div>
+                    <div className='tac'>
+                        <div className='seo-header'>Top Cities</div>
+                        <a>Hyderabad Bus Tickets</a>
+                        <a>Bangalore Bus Tickets</a>
+                        <a>Chennai Bus Tickets</a>
+                        <a>Pune Bus Tickets</a>
+                        <a>{'More >'}</a>
+                    </div>
+                    <div className='tac'>
+                        <div className='seo-header'>Top RTC's</div>
+                            <a>APSRTC</a>
+                            <a>MSRTC</a>
+                            <a>HRTC</a>
+                            <a>UPSRTC</a>
+                            <a>{'More>'}</a>
+                    </div>
+                    <div className='tac'>
+                        <div className='seo-header'>Other</div>
+                            <a>Other</a>
+                            <a>GSRTC</a>
+                            <a>RSRTC</a>
+                            <a>KTCL</a>
+                            <a>PEPSU</a>
+                            <a>{'More>'}</a>
+                    </div>
+                    <div className='tac'>
+                        <div className='seo-header'>Tempo Traveller in Cities</div>
+                        <a>Tempo Traveller in Cities</a>
+                        <a>Tempo traveller in Bangalore</a>
+                        <a>Tempo traveller in Chennai</a>
+                        <a>Tempo traveller in Mumbai</a>
+                        <a>Tempo traveller in Hyderabad</a>
+                        <a>Tempo traveller in Ahmedabad</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="seo-section">
+            <div className="main-body">
+                <div className='operator-footer-list'>
+                        <h6 className="seo-header">Top Operators</h6>
+                    <ul>
+                        <div className="trFlex">
+                            <div>
+                                <li><a>SRS Travels</a></li>
+                                <li><a>Evacay Bus</a></li>
+                                <li><a>Kallada Travels</a></li>
+                                <li><a>KPN Travels</a></li>
+                                <li><a>Orange Travels</a></li>
+                                <li><a>Parveen Travels</a></li>
+                                <li><a>Rajdhani Express</a></li>
+                                <li><a>VRL Travels</a></li>
+                                <li><a>Chartered Speed Bus</a></li>
+
+                            </div>
+                            <div>
+                                <li><a>Bengal Tiger</a></li>
+                                <li><a>SRM Travels</a></li>
+                                <li><a>Infant Jesus</a></li>
+                                <li><a>Patel Travels</a></li>
+                                <li><a>JBT Travels</a></li>
+                                <li><a>Shatabdi Travels</a></li>
+                                <li><a>Eagle Travels</a></li>
+                                <li><a>Kanker Roadways</a></li>
+                                <li><a>Komitla</a></li>
+
+                            </div>
+                            <div>
+                                <li><a>Sri Krishna Travels</a></li>
+                                <li><a>Humsafar Travels</a></li>
+                                <li><a>Mahasagar Travels</a></li>
+                                <li><a>Raj Express</a></li>
+                                <li><a>Sharma Travels</a></li>
+                                <li><a>Shrinath Travels</a></li>
+                                <li><a>Universal Travels</a></li>
+                                <li><a>Verma Travels</a></li>
+
+                            </div>
+                            <div>
+                                <li><a>Gujarat Travels</a></li>
+                                <li><a>Madurai Radha Travels</a></li>
+                                <li><a>Patel Tours and Travels</a></li>
+                                <li><a>Paulo Travels</a></li>
+                                <li><a>Royal Travels</a></li>
+                                <li><a>Amarnath Travels</a></li>
+                                <li><a>Vaibhav Travels</a></li>
+                                <li><a>Ganesh Travels</a></li>
+
+                            </div>
+                            <div>
+                                <li><a>Jabbar Travels</a></li>
+                                <li><a>Jain Travels</a></li>
+                                <li><a>Manish Travels</a></li>
+                                <li><a>Pradhan Travels</a></li>
+                                <li><a>YBM Travels</a></li>
+                                <li><a>Hebron Transports</a></li>
+                                <li><a>Mahalaxmi travels</a></li>
+                                <li><a>MR Travels</a></li>
+                                <li><a>Vivegam Travels</a></li>
+
+                            </div>
+                            <div>
+                                <li><a>VST Travels</a></li>
+                                <li><a>Jakhar Travels</a></li>
+                                <li><a>Kaleswari Travels</a></li>
+                                <li><a>Mahendra Travels</a></li>
+                                <li><a>Neeta Tours and Travels</a></li>
+                                <li><a>Yamani Travels</a></li>
+                                <li><a>Arthi Travels</a></li>
+
+                            </div>
+                        </div>
+                    </ul>
+
+                    <div className="more-link">
+                        <a className="more-link">{'All Operators >'}</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </>)
+}
