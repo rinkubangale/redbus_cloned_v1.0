b162f3fb4863a56d080aaa36c48514c9cda418e1
rinku <bangalerinku@gmail.com>
2021-09-28 16:25:36 +0530

    newComponent added

--- Front-end/redbus/src/App.js
+++ Front-end/redbus/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { Navbar } from "./Components/Navbar/Navbar";
 import Footer from "./Components/Footer/footer";
+import Number from "./Components/numberPage/number";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Number />
       <Footer />
     </div>
   );
