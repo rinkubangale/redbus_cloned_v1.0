9df22ef7805d157b145eddc4f9bff390de335a77
Bhaskar Rai <bhaskar9893@gmail.com>
2021-09-28 23:41:19 +0530

    login page by bhaskar

--- Front-end/redbus/src/App.js
+++ Front-end/redbus/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import { Navbar } from './Components/Navbar/Navbar';
+// import {Loginpage} from "./Components/LoginPage/Loginpage"
+
+
 
 function App() {
   return (
     <div>
       <Navbar/>
+    {/* <Loginpage/> */}
     </div>
   );
 }
