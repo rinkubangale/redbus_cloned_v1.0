6747c04e7d9c1aab60996b29d8d85f58b19f2edb
Bhaskar Rai <bhaskar9893@gmail.com>
2021-09-29 20:36:54 +0530

    r-pool page updated

---
+++ Front-end/redbus/src/Components/Rpool/Divone.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+
+import "./Divone.css";
+
+export default function Divone() {
+  return (
+    <div className="main_cont">
+      <div className="outer">
+        <div className="outstation_div">
+          <img
+            style={{ borderRight: "1px solid grey" }}
+            src="https://st.redbus.in/Images/rpools/rPoolIntracity.svg"
+            alt="loading"
+          ></img>
+          <img
+            src="https://st.redbus.in/Images/rpools/rPoolOutstation.svg"
+            alt="loading"
+          ></img>
+        </div>
+
+        <div className="text1">
+          <h2>Commuting within the City?</h2>
+        </div>
+        <div className="text2">
+          <h2>Travelling out of town?</h2>
+        </div>
+        <div className="text3">
+          <h3>Introducing Outstation</h3>
+        </div>
+      </div>
+
+      <div className="info_main_div">
+        <div className="info_flex">
+          <div>
+            <h2>Share your rides on rPool, </h2>
+          </div>
+          <div>
+            <p> Bikepool & Carpool for Professionals</p>
+          </div>
+        </div>
+        <div className="t1">
+          <p>
+            Intra-City services currently live in BANGALORE, HYDERABAD, PUNE,
+            NEW DELHI – NCR and CHENNAI
+          </p>
+        </div>
+        <div className="t2">
+          <p>Outstation services currently live across India</p>
+        </div>
+      </div>
+
+      <div className="app_click">
+        <button className="btn1">
+          <div>
+            <img
+              src="https://www.redbus.in/r-pool/images/googleplay.svg"
+              alt="loading"
+            ></img>
+          </div>
+        </button>
+
+        <button className="btn2">
+          <div>
+            <img
+              src="https://www.redbus.in/r-pool/images/ic_appstore.svg"
+              alt="loading"
+            ></img>
+          </div>
+        </button>
+      </div>
+    </div>
+  );
+}
--- Front-end/redbus/src/Pages/Rpool.jsx
+++ Front-end/redbus/src/Pages/Rpool.jsx
@@ -1,10 +1,13 @@
 import { Corporate } from '../Components/Rpool/Corporate';
 import { RAWs } from '../Components/Rpool/RAWs';
-
+import Divone from "../Components/Rpool/Divone";
+import Navigationbar from "../Components/Rpool/Navigationbar";
 
 export const Rpool = () => {
     return (
         <div>
+             <Divone/>
+             <Navigationbar />
             <Corporate />
             <RAWs/>
         </div>
