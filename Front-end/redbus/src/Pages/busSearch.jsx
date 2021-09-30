420d046affd5c128d1d5c8dd1d4798078ac89437
rinkubangale <bangalehemant@gmail.com>
2021-09-30 15:54:12 +0530

    new Component Added

--- Front-end/redbus/src/Pages/busSearch.jsx
+++ Front-end/redbus/src/Pages/busSearch.jsx
@@ -1,7 +1,10 @@
 import Statusbar from "../Components/nextPage/statusbar/statusbar";
+import Path from '../Components/nextPage/locationPath/path';
+
 
 export const BusSearch = () => {
     return (<>
         <Statusbar />
+        <Path />
     </>)
 }
