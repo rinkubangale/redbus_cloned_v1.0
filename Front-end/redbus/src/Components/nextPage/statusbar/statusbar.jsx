2cef53b8593e02b28a9ba4b82b9e715780ea13c3
rinkubangale <bangalehemant@gmail.com>
2021-09-30 13:55:58 +0530

    component added

--- Front-end/redbus/src/Components/nextPage/statusbar/statusbar.jsx
+++ Front-end/redbus/src/Components/nextPage/statusbar/statusbar.jsx
@@ -2,28 +2,47 @@ import styles from './statusbar.css'
 
 export default function tatusbar() {
     return (<>
-        <div>
+        <div style={{
+            backgroundColor: '#f8f4f4'
+                }}>
             <div>
                 <div>
                     <span>
-                        <ul>
+                        <ul className="st-flex">
                             <li>
-                                <h6>Home</h6>                            
+                                <h6>Home
+                                {' >  '}
+                                </h6>
                             </li>
                             <li>
-                                <h6>Bus Tickets</h6>                            
+                                <h6 style={{
+                                    fontWeight: '400',
+                                    pading: '10px'
+                                }}>Bus Tickets
+                                {' >  '}
+                                </h6>
                             </li>
                             <li>
-                                <h6>Nagpur Bus</h6>
+                                <h6 style={{
+                                    fontWeight: '400'
+                                }}>Nagpur Bus
+                                {' >  '}
+                                </h6>
                             </li>
                             <li>
-                                <h6>Nagpur To Pune Bus</h6>
+                                <h6 style={{
+                                    color: 'red',
+                                    fontWeight: '400'
+                                }}>Nagpur To Pune Bus</h6>
                             </li>
                         </ul>
                     </span>
                 </div>
                 <div>
-                    <span>Fare Starts From INR 800</span>
+                    <span style={{
+                        paddingLeft: '1em',
+                        fontSize: '14px'
+                    }}>Fare Starts From INR 800</span>
                 </div>
             </div>
         </div>
