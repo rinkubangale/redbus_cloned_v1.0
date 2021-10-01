2d008c42783b613fd27ae0d4b8f18e61d3a16f3c
rinkubangale <bangalehemant@gmail.com>
2021-10-01 14:10:13 +0530

    newLeftBar added in BusTicket Page

--- Front-end/redbus/src/Components/HomePage/Busticket/Header.jsx
+++ Front-end/redbus/src/Components/HomePage/Busticket/Header.jsx
@@ -7,7 +7,7 @@ const Header = ({departure,arrival,date}) => {
   return (
     <div className={styles.Header}>
       <div className={styles.HeaderOne}>
-        <p>
+        <p style={{ marginTop:'55px'}}>
           <span style={{ fontWeight: "bold", color: "#626367" }}> Home </span>
           &gt; Bus Tickets &gt; {departure} Bus &gt;
           <span style={{ color: "#ea4c53" }}>
